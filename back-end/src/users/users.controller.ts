463057dc1e7adbac3cd4931c02ba6738a25181b6
lamed0 <memedlamrani@gmail.com>
2025-12-28 23:48:22 +0100

    docker

--- back-end/src/users/users.controller.ts
+++ back-end/src/users/users.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
     @Get()
     findAll(): string{
         return "Show users.";
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string){
+        return `user id: ${id}`;
     }
 }
